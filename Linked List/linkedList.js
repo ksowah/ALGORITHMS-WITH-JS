5dc975e6db698811ceb6d996194fbfe389a03b48
ksowah <sowahkelvin640@gmail.com>
2022-03-19 22:48:21 +0000

    add insertnode method to linked list class

--- Linked List/linkedList.js
+++ Linked List/linkedList.js
@@ -4,3 +4,30 @@ class Node {
         this.next = next
     }
 }
+
+class LinkedList{
+    constructor(){
+        this.head = null
+        this.size = 0
+    }
+
+    // isert first node method
+    insertNode(data){
+        this.head = new Node(data, this.head)
+        this.size++
+    }
+
+    // insert to the last index
+
+    // insert to specific index
+
+    // clear list data
+
+    // print linked list data
+}
+
+const list = new LinkedList()
+
+list.insertNode(100)
+
+console.log(list);
