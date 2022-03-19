c29d3068889b6fec4fc64ce91d49830cd1492767
ksowah <sowahkelvin640@gmail.com>
2022-03-19 22:56:10 +0000

    add insert node to last index

--- Linked List/linkedList.js
+++ Linked List/linkedList.js
@@ -18,8 +18,13 @@ class LinkedList{
     }
 
     // insert to the last index
-    insertToLastIndex(){
-
+    insertToLastIndex(data){
+        const node = new Node(data)
+        let current = this.head
+        while(current.next){
+            current = current.next
+        }
+        current.next = node
     }
 
     // insert to specific index
@@ -42,6 +47,9 @@ list.insertNode(100)
 list.insertNode(120)
 list.insertNode(34)
 list.insertNode(42)
+
+// insert to last index
+list.insertToLastIndex(88)
 
 // print list data
 list.printListData()
